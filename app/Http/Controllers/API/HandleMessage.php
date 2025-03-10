2dd0d29bd3ac19b264b4d24d43671051109508b0
Franco Canevali <francocanevali@gmail.com>
2025-03-10 01:34:09 +0000

    please be the latest

--- app/Http/Controllers/API/HandleMessage.php
+++ app/Http/Controllers/API/HandleMessage.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\WhatsappSticker;
+use Illuminate\Support\Facades\Http;
 
 class HandleMessage extends Controller
 {
